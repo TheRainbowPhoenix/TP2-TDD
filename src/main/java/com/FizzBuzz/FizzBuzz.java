878bc9d2391c2705757af1555f58015c3a272a5e
Pho3 <rainbowdashtheone@gmail.com>
2018-11-27 11:45:55 +0100

    Adding gradle build files and setting up JUnit (with a passing test o/)

---
+++ src/main/java/com/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,14 @@
+/**
+ * @Author: uapv1701795
+ * @Date:   2018-11-27T11:05:27+01:00
+ * @Last modified by:   uapv1701795
+ * @Last modified time: 2018-11-27T11:45:26+01:00
+ */
+
+package com.FizzBuzz;
+
+public class FizzBuzz {
+  public String getResult(int i) {
+    return "1";
+  }
+}
---
+++ src/test/java/com/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+/**
+ * @Author: uapv1701795
+ * @Date:   2018-11-27T11:05:16+01:00
+ * @Last modified by:   uapv1701795
+ * @Last modified time: 2018-11-27T11:45:19+01:00
+ */
+
+ package com.FizzBuzz;
+
+ import static org.junit.jupiter.api.Assertions.assertEquals;
+
+ import org.junit.jupiter.api.DisplayName;
+ import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.params.ParameterizedTest;
+ import org.junit.jupiter.params.provider.CsvSource;
+
+
+class FizzBuzzTest {
+  @Test
+  void shouldPass() {
+    System.out.println("Vous passerez !");
+  }
+ }
